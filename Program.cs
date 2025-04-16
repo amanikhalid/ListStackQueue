d844ec06d589fd1dca941d47375c24aa8d40e478
Amani Khalid <amanikhalidd@gmail.com>
2025-04-16 10:29:05 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ListTasks
+{
+    internal class Program
+    {
+        static List<int> Numbers = new List<int> ();
+        static List<string> Names = new List<string>();
+        static List<int> Elements = new List<int>();
+        static List<string> Words = new List<string>();
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
